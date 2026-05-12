f38bba70b4346193868a55ea571699bbf44edc56
ekeabasiekiko <ekeabasiekiko@outlook.com>
2026-05-12 02:18:52 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>CI/CD Assignment Website</h1>
+      <p>Built with React and deployed automatically by Ekeabasi Ekiko.</p>
+    </div>
+  )
+}
+
+export default App
