474ce3fffa2c2d50e592453592952b35ac6e142f
ekeabasiekiko <ekeabasiekiko@outlook.com>
2026-05-12 10:38:55 +0100

    Added CI/CD workflow

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ function App() {
   return (
     <div>
       <h1>CI/CD Assignment Website</h1>
-      <p>Built with React and deployed automatically by Ekeabasi Ekiko.
-	  Updated Automatically by CI/CD
-	  </p>
+      <p>Built with React and deployed automatically by Ekeabasi Ekiko.  Updated Automatically by CI/CD</p>
     </div>
   )
 }
